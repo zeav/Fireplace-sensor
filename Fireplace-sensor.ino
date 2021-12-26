7ab03dd1765abbe02d62d137f778e166f28e6f46
Arne Velle <av@me-group.no>
2021-12-26 19:23:34 +0100

    Initial commit

---
+++ Fireplace-sensor.ino
@@ -0,0 +1,14 @@
+const int LDR_PIN = A0;
+
+const int reads = 50;
+const int timeBetweenReads = 100;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  //analogRead(LDR_PIN);
+  for
+}
