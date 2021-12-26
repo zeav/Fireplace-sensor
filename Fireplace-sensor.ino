eb90708a215f88eb3d373043c1dd5bcfc547c372
Arne Velle <av@me-group.no>
2021-12-26 19:49:48 +0100

    First test, only serial output

--- Fireplace-sensor.ino
+++ Fireplace-sensor.ino
@@ -3,12 +3,35 @@ const int LDR_PIN = A0;
 const int reads = 50;
 const int timeBetweenReads = 100;
 
+int readings[reads];
+int highestVarience;
+int currentVarience;
+
 void setup() {
   // put your setup code here, to run once:
-  
+  Serial.begin(115200);
 }
 
 void loop() {
-  //analogRead(LDR_PIN);
-  for
+  highestVarience = 0;
+  
+  //Get readings for (reads * timeBetweenReads) milliseconds
+  for (int i = 0; i < reads; i++)
+  {
+    readings[i] = analogRead(LDR_PIN);
+    delay(timeBetweenReads);
+  }
+
+  //Get highest varience between readings (highest 'flicker')
+  for(int i = 0; i < reads-1; i++)
+  {
+    currentVarience = abs(readings[i] - readings[i + 1]);
+    if(currentVarience > highestVarience)
+    {
+      highestVarience = currentVarience;
+    }
+  }
+
+  Serial.print(highestVarience);
+  
 }
